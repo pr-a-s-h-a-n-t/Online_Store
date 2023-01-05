66f6ec529168b1797e0c65c361fc9acfa8f0149a
Prashant Mishra <prashantmishra111@outlook.com>
2023-01-06 03:26:50 +0530

    singnIn page added, firebase install , config added, UUid install!!!

--- onstore/src/components/pages/Account/index.js
+++ onstore/src/components/pages/Account/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Notification } from "../../../utils/Notifications";
 
 function Profile() {
+  const[showNotifications, setShowNotification] = React.useState(false);
+
+useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+   
+    //   setShowNotification(true);
+      Notification ({
+        message: `Welcome ${user.name}!`,
+        type: 'success',
+      })
+    } 
+})
+
+
   return (
     <div>this is Profilepage</div>
   )
 }
 
 export default Profile
+
+ 
