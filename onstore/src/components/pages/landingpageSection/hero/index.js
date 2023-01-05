77f35a6104953088db7a6d467454288a5af9cf8c
Prashant Mishra <prashantmishra111@outlook.com>
2023-01-05 19:35:09 +0530

    framer-motion,react-confetti, and hero section added!!!

--- onstore/src/components/pages/landingpageSection/hero/index.js
+++ onstore/src/components/pages/landingpageSection/hero/index.js
@@ -1,20 +1,80 @@
-import React from 'react'
-import girlImg from "../../../../assets/girlImg.jpg"
-import "./Hero.css"
-import shoppingGirl from "../../../../assets/shoppingGirl.png"
-import shoppingBasket from "../../../../assets/shoppingBasket.png"
+import React, { useEffect } from "react";
+import { Button, Grid, Typography } from "@mui/material";
+
+import girlImg from "../../../../assets/girlImg.jpg";
+import "./Hero.css";
+import shoppingGirl from "../../../../assets/shoppingGirl.png";
+import shoppingBasket from "../../../../assets/shoppingBasket.png";
+
+import { motion } from "framer-motion";
+import Confetti from "react-confetti";
 
 function Hero() {
-  return (
-    <div  className="section4-main">
-    <div>
-       <h1> Sale is Live </h1>
-       <img src={shoppingBasket} alt="" />
-    </div>
-    
-    </div>
+  const [celebrations, setCelebrations] = React.useState(false);
 
-  )
+  useEffect(() => {
+    setCelebrations(true);
+    setTimeout(() => {
+      setCelebrations(false);
+    }, 10000);
+
+    //  return setCelebrations(false);
+  }, []);
+
+  return (
+    <>
+      {celebrations ? (
+        <Confetti
+          drawShape={(ctx) => {
+            ctx.beginPath();
+            for (let i = 0; i < 22; i++) {
+              const angle = 0.35 * i;
+              const x = (0.2 + 1.5 * angle) * Math.cos(angle);
+              const y = (0.2 + 1.5 * angle) * Math.sin(angle);
+              ctx.lineTo(x, y);
+            }
+            ctx.stroke();
+            ctx.closePath();
+          }}
+        />
+      ) : (
+        ""
+      )}
+
+      <div className="flex-wrapper">
+        <div className="heading-container">
+          <motion.h1
+            className="big-heading"
+            initial={{ opacity: 0, y: 50 }}
+            // animate={{ opacity: 4, y: 0 }}
+            animate={{ rotate: -370 }}
+            transition={{ type: " spring ", duration: 1 }}
+          >
+            <span className="stroke">
+              <span>New</span> Year
+            </span>
+            <br />
+            <span className="sale">Sale</span>
+          </motion.h1>
+        </div>
+
+        <div className="img-box">
+          <motion.img
+            src={shoppingBasket}
+            className="shoppingBasket"
+            initial={{ y: -10 }}
+            animate={{ y: 10 }}
+            transition={{
+              type: "smooth",
+              repeatType: "mirror",
+              duration: 2,
+              repeat: Infinity,
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
