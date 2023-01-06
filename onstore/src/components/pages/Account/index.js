4973d0faafac403d55f0e85aaf9667c2633be64d
Prashant Mishra <prashantmishra111@outlook.com>
2023-01-06 23:53:28 +0530

    signIn with google added, user data is saved in local storage and in firebase!!!

--- onstore/src/components/pages/Account/index.js
+++ onstore/src/components/pages/Account/index.js
@@ -1,27 +1,502 @@
-import React, { useEffect } from 'react'
 import { Notification } from "../../../utils/Notifications";
 
-function Profile() {
-  const[showNotifications, setShowNotification] = React.useState(false);
+import { Label } from "@mui/icons-material";
+import { Button, Divider, Grid, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db, auth, storage } from "../../../firebaseConfig/index";
 
-useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user) {
-   
-    //   setShowNotification(true);
-      Notification ({
-        message: `Welcome ${user.name}!`,
-        type: 'success',
-      })
-    } 
-})
+import "./Profile.css";
 
+import { useNavigate } from "react-router-dom";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
-  return (
-    <div>this is Profilepage</div>
-  )
-}
+import { async } from "@firebase/util";
+// import loadinglogo from " ../../../src/assets/loadingLogo.gif";
+// import loadingDark from " ../../../src/assets/loadingDark.gif";
 
-export default Profile
+import { DarkmodeContext } from "../../../contex/darkmode/index";
 
  
+
+function EmployerProfile() {
+  const [state, dispatch] = React.useContext(DarkmodeContext );
+
+  const navigateEmployer = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user"));
+  const uid = user.uid;
+  const [uploadLoading, setUploadLoading] = useState(0);
+  let inputRef = React.createRef();
+  const [disableField, setDisableField] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [values, setValues] = useState({
+    //should be fetched from firestore if the user has already created a profile
+    companyName: "",
+    companyWebsites: "",
+    companyEmail: "",
+    companyPhone: "",
+    industryType: "",
+    noOfEmployees: "",
+
+    companyLocation: "",
+    companyTagline: "",
+    companyDescription: "",
+    logo: "",
+  });
+
+  useEffect(() => {
+    //fetch data from firestore
+    //if data exists then set the values
+    setLoading(true);
+    let user = JSON.parse(localStorage.getItem("user"));
+    let uid = user.uid;
+    let docRef = doc(db, "userInfo", uid);
+    getDoc(docRef).then((doc) => {
+      // console.log(doc);
+      if (doc.exists()) {
+        // console.log("Document data:", doc.data());
+        setValues({ ...doc.data() });
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        Notification({ message: "No profile found", type: "danger" });
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  const submit = async (e) => {
+    e.preventDefault();
+    // console.log(values);s
+
+    //call firebase function to create employer profile
+    //store in firestore collection (userInfo)
+    //create a doc with docId = uid
+
+    // setDoc(docInfo,data)
+    //docInfo= doc(database,collection name, docId)
+    // try {
+    //   await setDoc(doc(db, "userInfo", uid), {
+    //     ...values,
+    //     type: "employer",
+    //   });
+    //   Notification({ message: "profile created successfully" });
+    //   navigateEmployer("/employer/profile");
+    // } catch (err) {
+    //   console.log(err);
+    //   Notification({ message: "something went wrong" });
+    // }
+  };
+
+  const uploadLogo = (e) => {
+    let file = e.target.files[0];
+    // console.log(file);s
+    //ref(storage,'path to file',file,name)
+    const storageRef = ref(storage, "company-logo/" + file.name);
+    //uploadBytesResumable(storage-Ref,file)
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setUploadLoading(progress);
+      },
+      (error) => {
+        Notification({ message: "something went wrong" });
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setValues({
+            ...values,
+            logo: downloadURL,
+          });
+          Notification({ message: "file uploaded successfully" });
+          setUploadLoading(0);
+        });
+      }
+    );
+
+    // upload file to firebase storage
+    // get the url of the file
+    // set the url to the logo value state
+  };
+  const makeEditable = async () => {
+    if (disableField) {
+      setDisableField(false);
+    } else {
+      setDisableField(true);
+      //
+      //call firebase function to update employer profile
+
+      try {
+        await setDoc(
+          doc(db, "userInfo", uid),
+          {
+            ...values,
+          },
+          { merge: true },
+          Notification({
+            message: "profile created successfully",
+            type: "success",
+          })
+        );
+      } catch (err) {
+        Notification({ message: "something went wrong" });
+      }
+    }
+  };
+
+  const logout = () => {
+    auth.signOut();
+    dispatch({ type: "LOGOUT" });
+    navigateEmployer("/employer/auth");
+  };
+  return (
+    <div
+      sx={{
+        color: state.shades.secondary,
+        backgroundColor: state.shades.primary,
+      }}
+    >
+      {loading ? (
+        <div>
+          <img
+            style={{ width: "100%", height: "100vh" }}
+            // src={loadinglogo}
+            alt="loading"
+          />
+        </div>
+      ) : (
+        <form
+          style={{
+            color: state.shades.secondary,
+            backgroundColor: state.shades.primary,
+          }}
+          onSubmit={(e) => submit(e)}
+          className="onboarding-container"
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={2}>
+                  <div className="upload-btn-container">
+                    {uploadLoading > 0 && uploadLoading <= 100 ? (
+                      <div>Loading {uploadLoading} %</div>
+                    ) : (
+                      <>
+                        <input
+                          accept="image/*"
+                          style={{
+                            display: "none",
+                          }}
+                          ref={inputRef}
+                          type={"file"}
+                          value={""}
+                          onChange={(e) => uploadLogo(e)}
+                        />
+                        <Button
+                          sx={{ display: disableField ? "none" : "block" }}
+                          onClick={() => inputRef.current.click()}
+                        >
+                          Upoad Logo
+                        </Button>
+                        {values.logo && (
+                          <div
+                            className="logo-img"
+                            style={{
+                              backgroundImage: `url(${values.logo})`,
+
+                              // background-size: 120%;
+                            }}
+                          ></div>
+                        )}
+                      </>
+                    )}
+                  </div>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                  <TextField
+                    InputLabelProps={{
+                      sx: {
+                        color: state.shades.secondary,
+                      },
+                    }}
+                    inputProps={{
+                      style: {
+                        color: state.shades.secondary,
+                      },
+                    }}
+                    sx={{
+                      "& .MuiInputBase-input.Mui-disabled": {
+                        WebkitTextFillColor: state.shades.secondary,
+                      },
+                    }}
+                    disabled={disableField}
+                    required
+                    size="small"
+                    fullWidth
+                    value={values.companyName}
+                    onChange={(e) =>
+                      setValues({ ...values, companyName: e.target.value })
+                    }
+                  />
+
+                  <TextField
+                    InputLabelProps={{
+                      sx: {
+                        color: state.shades.secondary,
+                      },
+                    }}
+                    inputProps={{
+                      style: {
+                        color: state.shades.secondary,
+                      },
+                    }}
+                    sx={{
+                      "& .MuiInputBase-input.Mui-disabled": {
+                        WebkitTextFillColor: state.shades.secondary,
+                      },
+                    }}
+                    disabled={disableField}
+                    size="small"
+                    fullWidth
+                    value={values.companyTagline}
+                    onChange={(e) =>
+                      setValues({ ...values, companyTagline: e.target.value })
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12} md={2}>
+                  <div
+                    style={{
+                      color: state.shades.secondary,
+                      backgroundColor: state.shades.primary,
+                    }}
+                    className="btn-container"
+                  >
+                    <Button onClick={makeEditable}>
+                      {" "}
+                      {disableField ? "Edit" : "save"}
+                    </Button>
+                    <Button onClick={logout}>Logout</Button>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Divider />
+            <Grid item xs={12} sm={6}>
+              <label className="field-label">Company Email</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={true}
+                size="small"
+                type="email"
+                required
+                fullWidth
+                value={values.companyEmail}
+                onChange={(e) =>
+                  setValues({ ...values, companyEmail: e.target.value })
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <label className="field-label">Company Phone</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={disableField}
+                size="small"
+                required
+                fullWidth
+                value={values.companyPhone}
+                onChange={(e) =>
+                  setValues({ ...values, companyPhone: e.target.value })
+                }
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label className="field-label">Company Website</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={disableField}
+                size="small"
+                fullWidth
+                value={values.companyWebsites}
+                onChange={(e) =>
+                  setValues({ ...values, companyWebsites: e.target.value })
+                }
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label className="field-label">Company Location</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={disableField}
+                size="small"
+                fullWidth
+                value={values.companyLocation}
+                onChange={(e) =>
+                  setValues({ ...values, companyLocation: e.target.value })
+                }
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label className="field-label">Industry Type</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={disableField}
+                size="small"
+                fullWidth
+                value={values.industryType}
+                onChange={(e) =>
+                  setValues({ ...values, industryType: e.target.value })
+                }
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label className="field-label">No. of employees</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={disableField}
+                size="small"
+                fullWidth
+                value={values.noOfEmployees}
+                onChange={(e) =>
+                  setValues({ ...values, noOfEmployees: e.target.value })
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <label className="field-label">Company description</label>
+              <TextField
+                InputLabelProps={{
+                  sx: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                inputProps={{
+                  style: {
+                    color: state.shades.secondary,
+                  },
+                }}
+                sx={{
+                  "& .MuiInputBase-input.Mui-disabled": {
+                    WebkitTextFillColor: state.shades.secondary,
+                  },
+                }}
+                disabled={disableField}
+                multiline
+                minRows={5}
+                fullWidth
+                value={values.companyDescription}
+                onChange={(e) =>
+                  setValues({ ...values, companyDescription: e.target.value })
+                }
+              />
+            </Grid>
+          </Grid>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default EmployerProfile;
--- onstore/src/components/pages/authPage/index.js
+++ onstore/src/components/pages/authPage/index.js
@@ -1,5 +1,5 @@
 import "./Auth.css";
-import React from "react";
+import React, { useState, useEffect, useDebugValue } from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 //  import setDoc , getDoc, db from firebase
@@ -10,27 +10,75 @@ import { Button, Grid } from "@mui/material";
 import { auth } from "../../../firebaseConfig/index";
 import GoogleLogo from "../../../assets/GoogleLogo.png";
 import { Notification } from "../../../utils/Notifications";
+ 
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 export default function UserAuth() {
+  const [customerData, setCustomerData] = useState({
+    userName: "",
+    userEmail: "",
+    userImage: "",
+    userPhone: "",
+  });
+ 
   const navigateUser = useNavigate();
-
+ 
   const signIn = () => {
     const provider = new GoogleAuthProvider();
-
+ 
     signInWithPopup(auth, provider)
-      .then((result) => {
+      .then(async (result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        // The signed-in user info.
         const user = result.user;
+        const uid = user.uid;
         localStorage.setItem("user", JSON.stringify(user));
-        navigateUser("/profile");
+          // we cant do this directly because state will not update it will show
+        // previous set values ex(
+          // in customerData i have set initial value userName: "",userEmail: "",userImage: "",userPhone: "",
+          // so this values will not update in firestore because this is how 
+          // react stat works.
+          // ******* Important********
+          // we are creating new object and then assiging this value to 
+          // setUserData .
+          // After that uploading   userData to firestore with collection name user userInfo.
+          
+   
+        const userData = {
+          userName: user.displayName,
+          userEmail: user.email,
+          userImage: user.photoURL,
+        };
+
+        // console.log( "test", userData.userName, userData.userEmail, userData.userImage);
+
+        setCustomerData(userData);
+        console.log("user data has been saved in local state", userData);
+ 
+        try {
+          await setDoc(doc(db, "userInfo", uid), {
+            ...userData,
+            type: "customer",
+          });
+          Notification({ message: "profile created successfully" });
+          navigateUser("/profile");
+          console.log("data saved in firebase", userData);
+        } catch (err) {
+          console.log(err);
+          Notification({ message: "something went wrong", type: "danger" });
+        }
+ 
         console.log(result, "result ");
       })
       .catch((error) => {
         console.log(error);
       });
   };
-
+ 
   return (
     <Grid container display="flex" flexDirection="column" marginTop="4rem">
       <Grid
@@ -77,3 +125,4 @@ export default function UserAuth() {
     </Grid>
   );
 }
+ 
